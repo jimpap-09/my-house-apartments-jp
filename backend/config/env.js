eacd9a88800dbd63bdd1a1dcce33d91af76aaf18
jimpap09 <jimpap09@github.com>
2026-07-14 17:24:50 +0300

    rename script drop-schema and fix single .env file in config/env.js

--- backend/config/env.js
+++ backend/config/env.js
@@ -1,15 +1,17 @@
 const path = require('path')
 const dotenv = require('dotenv')
 
-let envFile
+let envFile = '.env'
 
-if (process.env.NODE_ENV === 'local') {
-  envFile = '.env.local'
-} else if (process.env.NODE_ENV === 'production') {
-  envFile = '.env.prod'
-} else {
-  envFile = '.env.dev'
-}
+// if (process.env.NODE_ENV === 'local') {
+//   envFile = '.env.local'
+// } else if (process.env.NODE_ENV === 'production') {
+//   envFile = '.env.prod'
+// } else if (process.env.NODE_ENV === 'development') {
+//   envFile = '.env.dev'
+// } else {
+//   envFile = '.env'
+// }
 
 dotenv.config({
   path: path.resolve(__dirname, '..', envFile),
